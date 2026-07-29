f15368f73c73f6d9f901e91e6eb67d0f43b59d6e
Dovicvincent93 <vincentdove4@gmail.com>
2026-07-29 13:32:11 +0100

    Remove duplicate homepage from sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sitemap from "vite-plugin-sitemap";
+
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-  hostname: "https://meditrustnigeria.com",
-}),
+      hostname: "https://www.meditrustnigeria.com",
+      dynamicRoutes: [
+        "/doctors",
+        "/departments",
+        "/appointment",
+        "/contact",
+      ],
+    }),
   ],
 });
