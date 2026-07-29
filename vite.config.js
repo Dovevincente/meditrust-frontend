c6f42f5a1d2cffddb39f7558ffe0168ea64d1cc3
Dovicvincent93 <vincentdove4@gmail.com>
2026-07-29 13:21:47 +0100

    Add sitemap support for SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+  hostname: "https://meditrustnigeria.com",
+}),
+  ],
+});
